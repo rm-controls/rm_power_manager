4dd1282c2363d87065d3581ba9de2b6682d98c99
Lao·Zhu <2208213223@qq.com>
2021-03-23 23:33:03 +0800

    Modify DAC init function to make sure dac channel output is 0v just after initialized.

--- hardware/dac.c
+++ hardware/dac.c
@@ -12,7 +12,8 @@ void DAC_Config(void) {
     sConfig.DAC_ConnectOnChipPeripheral = DAC_CHIPCONNECT_DISABLE;
     sConfig.DAC_UserTrimming = DAC_TRIMMING_FACTORY;
     while (HAL_DAC_ConfigChannel(&hdac1, &sConfig, DAC_CHANNEL_1) != HAL_OK);
-    while (HAL_DAC_ConfigChannel(&hdac1, &sConfig, DAC_CHANNEL_2) != HAL_OK);
+    HAL_DAC_Start(&hdac1, DAC_CHANNEL_1);
+    HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 0x00);
 }
 
 void HAL_DAC_MspInit(DAC_HandleTypeDef *dacHandle) {
@@ -21,11 +22,10 @@ void HAL_DAC_MspInit(DAC_HandleTypeDef *dacHandle) {
     if (dacHandle->Instance == DAC1) {
         /**DAC1 GPIO Configuration
             PA4     ------> DAC1_OUT1
-            PA5     ------> DAC1_OUT2
         */
         __HAL_RCC_DAC12_CLK_ENABLE();
         __HAL_RCC_GPIOA_CLK_ENABLE();
-        GPIO_InitStruct.Pin = GPIO_PIN_4 | GPIO_PIN_5;
+        GPIO_InitStruct.Pin = GPIO_PIN_4;
         GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
         GPIO_InitStruct.Pull = GPIO_NOPULL;
         HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
