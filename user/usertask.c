30b7e02fdc697a0194e3e61018722d7ded8598fa
LaoZhu <2208213223@qq.com>
2021-07-29 09:44:24 +0800

    Increase the charging cut-off threshold and try to fix the problem that the super capacitor can't quit continuous charging.

--- user/usertask.c
+++ user/usertask.c
@@ -31,7 +31,7 @@ void UserTask(void *pvParameters) {
                         FSM_Status.FSM_Mode = OverPower_Mode;
                     break;
                 case SucapTest_Optimized:FSM_Status.FSM_Mode = SucapTest_Mode;
-                    while (Capacitor_Percent < 0.9f || Setting_FSM_Mode == SucapTest_Optimized)
+                    while (Capacitor_Percent < 0.9f && Setting_FSM_Mode == SucapTest_Optimized)
                         Delayms(1);
                     FSM_Status.FSM_Mode = Halt_Mode;
                     while (Setting_FSM_Mode == SucapTest_Optimized)
