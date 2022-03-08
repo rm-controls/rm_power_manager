c0b70326d90a77fda0ff5e452845339c209ef11a
582864732 <582864732@qq.com>
2022-03-08 19:49:48 +0800

    fixup! be able to transmit data to NUC without receiving data from referee system

--- forms/settings_form.c
+++ forms/settings_form.c
@@ -4,14 +4,40 @@
 
 #include "main.h"
 
-Button_Struct_t TurnBack_Button2;
-Numeric_Struct_t RTCYear_Numeric, RTCMonth_Numeric, RTCDate_Numeric;
-Numeric_Struct_t RTCHour_Numeric, RTCMinute_Numeric, RTCSecond_Numeric;
-Lable_Struct_t RTCDate_Lable, RTCTime_Lable;
+static Button_Struct_t TurnBack_Button2;
+static Button_Struct_t Normal_Botton, ChargeFirst_Botton, UseFrist_Botton,
+    SucapTest_Botton;//To switch Setting_OptiSchemes
+static Numeric_Struct_t RTCYear_Numeric, RTCMonth_Numeric, RTCDate_Numeric;
+static Numeric_Struct_t RTCHour_Numeric, RTCMinute_Numeric, RTCSecond_Numeric;
+static Lable_Struct_t RTCDate_Lable, RTCTime_Lable;
 
 void TurnBack_Button2_Callback(void *object, unsigned char key) {
-    Form_Info_Structure.Form_Index = Main_Form_Index;
-    MainForm_Init();
+  Form_Info_Structure.Form_Index = Main_Form_Index;
+  MainForm_Init();
+}
+
+void Normal_Botton_Callback(void *object, unsigned char key) {
+  Setting_OptiSchemes = Normal_Optimized;
+  Form_Info_Structure.Form_Index = Main_Form_Index;
+  MainForm_Init();
+}
+
+void ChargeFirst_Botton_Callback(void *object, unsigned char key) {
+  Setting_OptiSchemes = ChargeFirst_Optimized;
+  Form_Info_Structure.Form_Index = Main_Form_Index;
+  MainForm_Init();
+}
+
+void UseFirst_Botton_Callback(void *object, unsigned char key) {
+  Setting_OptiSchemes = UseFirst_Optimized;
+  Form_Info_Structure.Form_Index = Main_Form_Index;
+  MainForm_Init();
+}
+
+void SucapTest_Botton_Callback(void *object, unsigned char key) {
+  Setting_OptiSchemes = SucapTest_Optimized;
+  Form_Info_Structure.Form_Index = Main_Form_Index;
+  MainForm_Init();
 }
 
 void RTCYear_Numeric_Callback(void *object, unsigned char key) {
@@ -23,112 +49,51 @@ void SettingsForm_Update(void) {
 }
 
 void SettingsForm_Init(void) {
-    RTC_TimeTypeDef RTC_TimeStruct;
-    RTC_DateTypeDef RTC_DateStruct;
-    char String_Buffer_Tmp[16];
-    HAL_RTC_GetTime(&hrtc, &RTC_TimeStruct, RTC_FORMAT_BIN);
-    HAL_RTC_GetDate(&hrtc, &RTC_DateStruct, RTC_FORMAT_BIN);
-    GUI_Clear(C_WHITE);
+  GUI_Clear(C_WHITE);
 
-    Form_Info_Structure.Widget_Index = 0;
-    FirstWidget = (GUI_Object_Struct_t *) &TurnBack_Button2;
-    TurnBack_Button2.X_Pos = 24;
-    TurnBack_Button2.Y_Pos = 135;
-    TurnBack_Button2.Width = 80;
-    TurnBack_Button2.Height = 20;
-    TurnBack_Button2.Text = "Turn Back";
-    TurnBack_Button2.NextButton = &RTCYear_Numeric;
-    TurnBack_Button2.CallbackFunction = TurnBack_Button2_Callback;
+  Normal_Botton.X_Pos = 2;
+  Normal_Botton.Y_Pos = 2;
+  Normal_Botton.Width = 80;
+  Normal_Botton.Height = 22;
+  Normal_Botton.Text = "Normal";
+  Normal_Botton.NextButton = &ChargeFirst_Botton;
+  Normal_Botton.CallbackFunction = Normal_Botton_Callback;
 
-    RTCDate_Lable.X_Pos = 2;
-    RTCDate_Lable.Y_Pos = 8;
-    RTCDate_Lable.Color = C_BLACK;
-    RTCDate_Lable.Text = "Date";
+  ChargeFirst_Botton.X_Pos = 2;
+  ChargeFirst_Botton.Y_Pos = 26;
+  ChargeFirst_Botton.Width = 80;
+  ChargeFirst_Botton.Height = 22;
+  ChargeFirst_Botton.Text = "ChargeFirst";
+  ChargeFirst_Botton.NextButton = &UseFrist_Botton;
+  ChargeFirst_Botton.CallbackFunction = ChargeFirst_Botton_Callback;
 
-    memset(String_Buffer_Tmp, 0x00, sizeof(String_Buffer_Tmp));
-    sprintf(String_Buffer_Tmp, "20%02d", RTC_DateStruct.Year);
-    RTCYear_Numeric.X_Pos = 26;
-    RTCYear_Numeric.Y_Pos = 4;
-    RTCYear_Numeric.Width = 38;
-    RTCYear_Numeric.Height = 20;
-    RTCYear_Numeric.Value = RTC_DateStruct.Year;
-    RTCYear_Numeric.Text = (const char *) String_Buffer_Tmp;
-    RTCYear_Numeric.Widget_Type = Numeric_Widget_Type;
-    RTCYear_Numeric.NextNumeric = &RTCMonth_Numeric;
-    RTCYear_Numeric.CallbackFunction = RTCYear_Numeric_Callback;
-    GUI_InitNumeric(&RTCYear_Numeric);
+  UseFrist_Botton.X_Pos = 2;
+  UseFrist_Botton.Y_Pos = 50;
+  UseFrist_Botton.Width = 80;
+  UseFrist_Botton.Height = 22;
+  UseFrist_Botton.Text = "UseFirst";
+  UseFrist_Botton.NextButton = &SucapTest_Botton;
+  UseFrist_Botton.CallbackFunction = UseFirst_Botton_Callback;
 
-    memset(String_Buffer_Tmp, 0x00, sizeof(String_Buffer_Tmp));
-    sprintf(String_Buffer_Tmp, "%02d", RTC_DateStruct.Month);
-    RTCMonth_Numeric.X_Pos = 66;
-    RTCMonth_Numeric.Y_Pos = 4;
-    RTCMonth_Numeric.Width = 28;
-    RTCMonth_Numeric.Height = 20;
-    RTCMonth_Numeric.Value = RTC_DateStruct.Month;
-    RTCMonth_Numeric.Text = (const char *) String_Buffer_Tmp;
-    RTCMonth_Numeric.Widget_Type = Numeric_Widget_Type;
-    RTCMonth_Numeric.NextNumeric = &RTCDate_Numeric;
-    RTCMonth_Numeric.CallbackFunction = RTCYear_Numeric_Callback;
-    GUI_InitNumeric(&RTCMonth_Numeric);
+  SucapTest_Botton.X_Pos = 2;
+  SucapTest_Botton.Y_Pos = 74;
+  SucapTest_Botton.Width = 80;
+  SucapTest_Botton.Height = 22;
+  SucapTest_Botton.Text = "SucapTest";
+  SucapTest_Botton.NextButton = &TurnBack_Button2;
+  SucapTest_Botton.CallbackFunction = SucapTest_Botton_Callback;
 
-    memset(String_Buffer_Tmp, 0x00, sizeof(String_Buffer_Tmp));
-    sprintf(String_Buffer_Tmp, "%02d", RTC_DateStruct.Date);
-    RTCDate_Numeric.X_Pos = 96;
-    RTCDate_Numeric.Y_Pos = 4;
-    RTCDate_Numeric.Width = 28;
-    RTCDate_Numeric.Height = 20;
-    RTCDate_Numeric.Value = RTC_DateStruct.Date;
-    RTCDate_Numeric.Text = (const char *) String_Buffer_Tmp;
-    RTCDate_Numeric.Widget_Type = Numeric_Widget_Type;
-    RTCDate_Numeric.NextNumeric = &RTCHour_Numeric;
-    RTCDate_Numeric.CallbackFunction = RTCYear_Numeric_Callback;
-    GUI_InitNumeric(&RTCDate_Numeric);
+  TurnBack_Button2.X_Pos = 2;
+  TurnBack_Button2.Y_Pos = 134;
+  TurnBack_Button2.Width = 80;
+  TurnBack_Button2.Height = 22;
+  TurnBack_Button2.Text = "TurnBack";
+  TurnBack_Button2.NextButton = NULL;
+  TurnBack_Button2.CallbackFunction = TurnBack_Button2_Callback;
 
-    RTCTime_Lable.X_Pos = 2;
-    RTCTime_Lable.Y_Pos = 30;
-    RTCTime_Lable.Color = C_BLACK;
-    RTCTime_Lable.Text = "Time";
-
-    memset(String_Buffer_Tmp, 0x00, sizeof(String_Buffer_Tmp));
-    sprintf(String_Buffer_Tmp, "%d", RTC_TimeStruct.Hours);
-    RTCHour_Numeric.X_Pos = 26;
-    RTCHour_Numeric.Y_Pos = 26;
-    RTCHour_Numeric.Width = 30;
-    RTCHour_Numeric.Height = 20;
-    RTCHour_Numeric.Value = RTC_TimeStruct.Hours;
-    RTCHour_Numeric.Text = (const char *) String_Buffer_Tmp;
-    RTCHour_Numeric.Widget_Type = Numeric_Widget_Type;
-    RTCHour_Numeric.NextNumeric = &RTCMinute_Numeric;
-    RTCHour_Numeric.CallbackFunction = RTCYear_Numeric_Callback;
-    GUI_InitNumeric(&RTCHour_Numeric);
-
-    memset(String_Buffer_Tmp, 0x00, sizeof(String_Buffer_Tmp));
-    sprintf(String_Buffer_Tmp, "%d", RTC_TimeStruct.Minutes);
-    RTCMinute_Numeric.X_Pos = 60;
-    RTCMinute_Numeric.Y_Pos = 26;
-    RTCMinute_Numeric.Width = 30;
-    RTCMinute_Numeric.Height = 20;
-    RTCMinute_Numeric.Value = RTC_TimeStruct.Minutes;
-    RTCMinute_Numeric.Text = (const char *) String_Buffer_Tmp;
-    RTCMinute_Numeric.Widget_Type = Numeric_Widget_Type;
-    RTCMinute_Numeric.NextNumeric = &RTCSecond_Numeric;
-    RTCMinute_Numeric.CallbackFunction = RTCYear_Numeric_Callback;
-    GUI_InitNumeric(&RTCMinute_Numeric);
-
-    memset(String_Buffer_Tmp, 0x00, sizeof(String_Buffer_Tmp));
-    sprintf(String_Buffer_Tmp, "%d", RTC_TimeStruct.Seconds);
-    RTCSecond_Numeric.X_Pos = 94;
-    RTCSecond_Numeric.Y_Pos = 26;
-    RTCSecond_Numeric.Width = 30;
-    RTCSecond_Numeric.Height = 20;
-    RTCSecond_Numeric.Value = RTC_TimeStruct.Seconds;
-    RTCSecond_Numeric.Text = (const char *) String_Buffer_Tmp;
-    RTCSecond_Numeric.Widget_Type = Numeric_Widget_Type;
-    RTCSecond_Numeric.NextNumeric = NULL;
-    RTCSecond_Numeric.CallbackFunction = RTCYear_Numeric_Callback;
-    GUI_InitNumeric(&RTCSecond_Numeric);
-
-    GUI_InitLable(&RTCDate_Lable);
-    GUI_InitLable(&RTCTime_Lable);
-    GUI_InitButton(&TurnBack_Button2);
+  GUI_InitButton(&Normal_Botton);
+  GUI_InitButton(&ChargeFirst_Botton);
+  GUI_InitButton(&UseFrist_Botton);
+  GUI_InitButton(&SucapTest_Botton);
+  GUI_InitButton(&TurnBack_Button2);
 }
